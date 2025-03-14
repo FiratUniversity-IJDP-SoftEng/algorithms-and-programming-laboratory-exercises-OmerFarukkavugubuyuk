4417edeb3440c52c5bd77aa7d7a7550f76f7f684
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2025-03-14 14:44:22 +0300

    Update ex005.py

--- Exercises/ex005.py
+++ Exercises/ex005.py
@@ -1,2 +1,11 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543015
+# Your Name and Surname: Ömer Faruk Kavuğubüyük
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+hi_length = len("Hi")
+first_name_length = len(first_name)
+last_name_length = len(last_name)
+
+print("Hi"," "*first_name_length, last_name)
+print("  ", first_name)
