acd4bb35d0bd7ce46bebdd7362e67c36577eaffa
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2025-03-14 14:26:19 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543015
+# Your Name and Surname: Ömer Faruk Kavuğubüyük
+
+import sys
+
+print("Python version:", sys.version.split()[0])
+print("Version info:", sys.version_info)
