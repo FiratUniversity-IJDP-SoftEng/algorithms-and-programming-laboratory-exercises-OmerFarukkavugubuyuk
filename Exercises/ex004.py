bb8d3ec4325fe1707b072dca37d12fcbed42841e
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2025-03-14 14:37:49 +0300

    Update ex004.py

--- Exercises/ex004.py
+++ Exercises/ex004.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543015
+# Your Name and Surname: Ömer Faruk Kavuğubüyük
+import math
+
+radius = float(input("Input the radius of the circle: "))
+
+area = math.pi * radius ** 2
+
+print(f"The area of the circle with radius {radius} is: {area:.2f}")
