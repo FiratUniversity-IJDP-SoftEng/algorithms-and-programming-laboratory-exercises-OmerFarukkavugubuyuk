91323e67f6ba9abae7b3cbed8f464698c7cdc02a
OmerFarukkavugubuyuk <148456682+OmerFarukkavugubuyuk@users.noreply.github.com>
2025-03-14 14:34:02 +0300

    Update ex003.py

--- Exercises/ex003.py
+++ Exercises/ex003.py
@@ -1,2 +1,13 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543015
+# Your Name and Surname: Ömer Faruk Kavuğubüyük
+
+from datetime import datetime
+
+now = datetime.now()
+
+current_date = now.strftime("%Y-%m-%d")
+current_time = now.strftime("%H:%M:%S")
+
+print("Current date:", current_date)
+print("Current time:", current_time)
+print("Current date and time:", current_date , current_time)
